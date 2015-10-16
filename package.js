23c2615bab37409192c6b89d39c0c3e3089f050b
Karl STEIN <karl.stein.pro@gmail.com>
2015-10-16 12:32:38 -1000

    version 0.1.1 - added explicit dependencies - added LICENSE

--- package.js
+++ package.js
@@ -1,28 +1,26 @@
 Package.describe({
     name: 'jalik:ufs-gridfs',
-    version: '0.1.0',
+    version: '0.1.1',
     author: 'rodrigoknascimento@gmail.com',
     summary: 'GridFS store for UploadFS',
     homepage: 'https://github.com/jalik/jalik-ufs-gridfs',
     git: 'https://github.com/jalik/jalik-ufs-gridfs.git',
-    documentation: 'README.md'
+    documentation: 'README.md',
+    license: 'MIT'
 });
 
 Package.onUse(function (api) {
     api.versionsFrom('1.1.0.2');
-
-    api.use('underscore');
     api.use('check');
+    api.use('underscore');
     api.use('mongo');
     api.use('jalik:ufs@0.2.4');
-
     api.addFiles('ufs-gridfs.js');
 });
 
 Npm.depends({
     'gridfs-stream': '0.5.3'
 });
-
 
 Package.onTest(function (api) {
     api.use('tinytest');
