006070aceafb7a0719ec5378f724488f2c91db88
Karl Stein <karl.stein.pro@gmail.com>
2020-09-21 12:06:52 -1000

    Upgrades jalik:ufs to 1.0.3

--- package.js
+++ package.js
@@ -38,6 +38,6 @@ Package.onUse(function (api) {
   api.versionsFrom('1.9');
   api.use('ecmascript');
   api.use('mongo');
-  api.use('jalik:ufs@1.0.2');
+  api.use('jalik:ufs@1.0.3');
   api.mainModule('ufs-gridfs.js');
 });
